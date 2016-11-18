d72cbfcacc69405eaf5d1183b555ab4447600826
yoshimitsusano <ichigogari.shiohigari@outlook.jp>
2016-11-18 12:14:06 +0000

    8-7

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,25 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
+
   def index
     @message = Message.new
     @messages = Message.all
   end
+  
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
 
-   def create
+  def create
     @message = Message.new(message_params)
     if @message.save
       redirect_to root_path , notice: 'メッセージを保存しました'
@@ -15,11 +30,19 @@ class MessagesController < ApplicationController
       render 'index'
     end
   end
+   
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
 
   private
   def message_params
     params.require(:message).permit(:name, :body)
   end
-  ## ここまで
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
 end
 
